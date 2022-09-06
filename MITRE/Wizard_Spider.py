482e64f125531db5f59b4f7c294da1f054a9457e
sv3nb <91876383+sv3nb@users.noreply.github.com>
2022-09-06 19:58:14 +0200

    Add files via upload

---
+++ MITRE/Wizard_Spider.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# import a matrix filtered on Wizard_Spider 
+# The schema contains metadata about the columns
+
+import pandas as pd
+schema_df  = pd.read_json('MITRE_Schema.json')
+df = pd.read_json('Wizard_Spider.json')
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.max_rows', 50)
+schema_df.set_index('column', inplace=True )
+explanation = schema_df.loc['metadata', 'explanation']
+
+print(explanation)
+
+# filter with regex keywords
+
+content_filter = df['comment'].str.contains('FTP|network|enumeration|SMB|RDP', na=False, case=False, regex=True) # set regex flag and we can use OR
+df.loc[content_filter, ['tactic', 'techniqueID', 'comment', 'metadata']]
+
+# different approach to return the value matched when it is found in the 'comment' series.
+
+network_protocols = ['FTP', 'VPN', 'enumeration', 'SMB', 'RDP', 'RPC', 'Windows remote', 'RPC', 'Kerberos', 'WMI', 'SQL']
+def matcher(comment):
+    for protocol in network_protocols:
+        if protocol.lower() in comment.lower():
+            return f'network protocol used is: {protocol}'
+    else:
+        return 'none'
+
+df['metadata'] = df['comment'].apply(matcher)
+result = (df['metadata'] != 'none')
+df.loc[result, ['tactic', 'techniqueID', 'metadata']]
+
+# construct a hyperlink for all matching techniques
+
+def concat(technique):
+    technique = technique.replace('.', '/')
+    return f"https://attack.mitre.org/techniques/{technique}"
+
+df.loc[result, 'links']  = df.loc[result, 'techniqueID'].apply(concat)
+df.loc[result, ['tactic', 'techniqueID', 'metadata', 'links']]
+
+# create a list of keywords to search for in the column tactic
+
+tactics = ['initial-access', 'lateral-movement', 'exfiltration', 'collection', 'command-and-control']
+
+# and use them as a filter
+
+filt = (df['tactic'].isin(tactics))
+net_related = (df.loc[filt, ['tactic', 'techniqueID', 'comment', 'metadata']]) # create a new filtered dataframe with just these columns
+
---
+++ MITRE/webscraper.py
@@ -0,0 +1,47 @@
+# inspect element with chrome to find out the class so you can select specific elements from the HTML page
+import requests, bs4
+from lxml import etree
+    
+'''retrieves HTML from url to parse the description of the technique so we may extract useful information'''
+
+res = requests.get('https://attack.mitre.org/techniques/T1570/')
+res.raise_for_status()
+technique = bs4.BeautifulSoup(res.text, 'html.parser')
+
+# Full technique description
+
+description = technique.find_all("div", {"class": "description-body"})
+desc = technique.select('div.description-body')
+mylist = [(p.get_text(strip=True)) for p in desc]
+mylist[0]
+
+# Use XPATH to extract deep nested data
+# The second table of this class contains the <p> elements that describe the mitigations
+
+dom = etree.HTML(str(technique))
+root = technique.findAll('table', {'class': 'table table-bordered table-alternate mt-2'})
+try:
+    children = root[1].findChildren('p', recursive=True)
+except IndexError:
+    number = 0
+else:
+    number = len(children)
+
+mitigation_list = []
+
+# for certain (sub)techniques there is no mitigation
+
+if number == 0:
+    print("This type of attack technique cannot be easily mitigated")
+elif number <= 1:
+    mitigation_list.append(dom.xpath(f'//*[@id="v-attckmatrix"]/div[2]/div/div/div/table[2]/tbody/tr[{1}]/td[3]/p/text()'))
+else:
+    for i in range(1,number + 1): # added + 1 or else he misses the last mitigation.
+        try:
+            mitigation_list.append(dom.xpath(f'//*[@id="v-attckmatrix"]/div[2]/div/div/div/table[2]/tbody/tr[{i}]/td[3]/p/text()'))
+        except IndexError:
+            continue
+
+[print(mitigation[0]) for mitigation in mitigation_list]
+
+
