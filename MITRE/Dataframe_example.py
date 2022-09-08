f099d8d425f844bf637f89886478d95b7f84282d
sv3nb <91876383+sv3nb@users.noreply.github.com>
2022-09-08 17:18:27 +0200

    Update and rename Wizard_Spider.py to Dataframe_example.py

--- MITRE/Dataframe_example.py
+++ MITRE/Dataframe_example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# import a matrix filtered on Wizard_Spider 
+# import a matrix filtered on Wizard_Spider group
 # The schema contains metadata about the columns
 
 import pandas as pd
