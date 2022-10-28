2b1b783564a024a8e6654c0fe666244bfaa35235
sv3nb <91876383+sv3nb@users.noreply.github.com>
2022-10-28 11:45:05 +0200

    Create db_class.py

---
+++ MITRE/build_db/db_class.py
@@ -0,0 +1,35 @@
+# Create object to represent our database
+
+database = "technique_db.json"
+
+class TechniqueDatabase(object):
+    def __init__(self, name):
+        self._db = {}
+        if name is not None:
+            self.load(name)
+
+    def __iter__(self):
+        return iter(self._db)
+
+    def load(self, name):
+        with open(name) as tech_db:
+            self._db = json.load(tech_db)
+
+    def find(self, id):
+        return next(filter(lambda x: x["id"] == id, self), {})
+
+    # following functions allow us to retrieve properties from the DB
+    def get_name(self, id):
+        return self.find(id).get("name")
+
+    def get_traffic(self, id):
+        return self.find(id).get("traffic")
+
+    def get_proto(self, id):
+        return self.find(id).get("protocols")
+
+    def get_desc(self, id):
+        return self.find(id).get("description")
+
+    def get_mitigation(self, id):
+        return self.find(id).get("mitigation")
